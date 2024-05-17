073740dfa0334d2d48170ab81e69fe4da2ad8500
Widyo Tri Cahya Utomo <74693211+tcuber11@users.noreply.github.com>
2024-05-17 16:09:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,8 @@ if ingredients_list:
 
 
 st.title("📞 Contact me")
-st.write("""Whatsapp : [62 812 9598 1476] (http://wa.me/6281295981476)""")
-st.write("""Instagram : [tcu98] (https://instagram.com/tcu98)""")
+st.write("""Whatsapp : [62 812 9598 1476](http://wa.me/6281295981476)""")
+st.write("""Instagram : [tcu98](https://instagram.com/tcu98)""")
 st.write("""Email : widyo11@gmail.com""")
 
 st.write("""©️ Tcuber11""")
