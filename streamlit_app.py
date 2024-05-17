e5ae7352cf7ec22415dd23058e7a1e32c9cdd285
Widyo Tri Cahya Utomo <74693211+tcuber11@users.noreply.github.com>
2024-05-17 16:22:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,12 +44,12 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")    
 
-
-
-st.write(":telephone_receiver: Contact me :telephone_receiver:")
-st.write("""Whatsapp : [62 812 9598 1476](http://wa.me/6281295981476)""")
-st.write("""Instagram : [tcu98](https://instagram.com/tcu98)""")
-st.write("""Email : widyo11@gmail.com""")
+st.write("")
+st.write("")
+st.write(":telephone_receiver: Contact me")
+st.write(""":iphone: : [62 812 9598 1476](http://wa.me/6281295981476)""")
+st.write(""":camera: : [tcu98](https://instagram.com/tcu98)""")
+st.write(""":envelope_with_arrow: : widyo11@gmail.com""")
 
 st.write(""":copyright: Tcuber11""")
 
